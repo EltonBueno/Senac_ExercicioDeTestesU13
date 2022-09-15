897ec60568baa59e83d064656c85bf6b329be893
ELTON SENAC <elsbueno6@gmail.com>
2022-09-15 20:49:37 -0300

    Criando Testes

---
+++ ExercicioUC13.Core/ManipulandoStrings.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ExercicioUC13.Core
+{
+    public static class ManipulandoStrings
+    {
+        //Deve retornar o nome da "Olá " + o nome informado.
+        //Ex: input "Fulano", output: "Olá Fulano"
+        //Deve retornar vazio para nomes inválidos
+        public static string OlaPessoa(string nome)
+        {
+            if (string.IsNullOrEmpty(nome))
+                return string.Empty;
+
+            return $"Olá {nome}";
+        }
+
+        /*
+        Processar linhas de Log.
+        Toda linha de log segue esse padrão: "[<LEVEL>]: <MENSAGEM>".
+
+        Há 3 diferentes níveis:
+
+        INFO
+        WARNING
+        ERROR
+
+        Ex1: "[ERROR]: Operação inválida"
+        Ex2: "[WARNING]: Conexão limitada"
+        Ex3: "[INFO]: Operação concluída"
+         */
+
+
+        public static string ExtrairMensagemDoLog(string mensagem)
+        {
+            return mensagem.Substring(mensagem.IndexOf(":") + 1);
+        }
+
+        /* Removedor de espaços duplicado
+         * Devolve um texto sem duplos espaços, ou espaços extras no início e fim
+         * Ex: input: " Bom  dia", output: "Bom dia"
+         */
+        public static string RemoveEspacosDuplicados(string texto)
+        {
+            while (texto.Contains("  ")) 
+                texto = texto.Replace("  ", " ");
+
+            return texto;
+        }
+    }
+}
---
+++ ExercicioUC13.Test/ManipulandoStringTest.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using ExercicioUC13.Core;
+
+namespace ExercicioUC13.Test
+{
+    public class ManipulandoStringTest
+    {
+       
+        [Theory]
+        [InlineData("Pedro")]
+        [InlineData("Pedro Henrique")]
+        public void OlaPessoa_recebeNome_devolveBoasVindas(string nome)
+        {
+            //Arrange
+
+            //Act
+            var resultado = ManipulandoStrings.OlaPessoa(nome);
+            //Assert
+            Assert.Equal($"Olá {nome}", resultado);
+        }
+        
+        [Theory]
+        [InlineData ("")]
+        [InlineData ("Pedro ")]
+        [InlineData ("Pedro!")]
+        [InlineData ("!!")]
+        [InlineData ("12234")]
+        public void OlaPessoa_recebeNome_devolveVazio(string nome)
+        {
+            //Arrange
+
+            //Act
+            var resultado = ManipulandoStrings.OlaPessoa(nome);
+            //Assert
+            Assert.Equal("", resultado);
+        }
+        
+        
+
+        [Fact]
+        public void ExtrairMensagemDoLog_ExibirMensagemInfo()
+        {
+
+            //Arrange
+
+            //Act
+            var  resultado = ManipulandoStrings.ExtrairMensagemDoLog("[INFO]: Operação concluída");
+            //Assert
+            Assert.Equal("Operação concluída", resultado); 
+
+        } 
+        
+        [Fact]
+        public void ExtrairMensagemDoLog_ExibirMensagemError()
+        {
+
+            //Arrange
+
+            //Act
+            var  resultado = ManipulandoStrings.ExtrairMensagemDoLog("[ERROR]: Operação inválida");
+            //Assert
+            Assert.Equal("Operação inválida", resultado); 
+
+        } 
+        
+        [Fact]
+        public void ExtrairMensagemDoLog_ExibirMensagemWarning()
+        {
+
+            //Arrange
+
+            //Act
+            var  resultado = ManipulandoStrings.ExtrairMensagemDoLog("[WARNING]: Conexão limitada");
+            //Assert
+            Assert.Equal("Conexão limitada", resultado); 
+
+        }
+
+        [Fact]
+        public void RemoveEspacosDuplicados_RemoveEspacoDuplo()
+        {
+
+            //Arrange
+
+            //Act
+            var resultado = ManipulandoStrings.RemoveEspacosDuplicados("Bom  dia");
+
+            //Assert
+            Assert.Equal("Bom dia", resultado);
+
+
+        }
+
+        [Fact]
+        public void RemoveEspacosDuplicados_RemoveEspacoExtrasnoInicioEFIm()
+        {
+
+            //Arrange
+
+            //Act
+            var resultado = ManipulandoStrings.RemoveEspacosDuplicados("  Bom dia  ");
+
+            //Assert
+            Assert.Equal(" Bom dia ", resultado);
+
+
+        }
+        
+        [Fact]
+        public void RemoveEspacosDuplicados_RemoveEspaconoInicioEFIm()
+        {
+
+            //Arrange
+
+            //Act
+            var resultado = ManipulandoStrings.RemoveEspacosDuplicados(" Bom dia ");
+
+            //Assert
+            Assert.Equal("Bom dia", resultado);
+
+
+        }
+        
+        [Fact]
+        public void RemoveEspacosDuplicados_RemoveVariosEspacos()
+        {
+
+            //Arrange
+
+            //Act
+            var resultado = ManipulandoStrings.RemoveEspacosDuplicados(" Bom              dia ");
+
+            //Assert
+            Assert.Equal("Bom dia", resultado);
+
+
+        }
+
+    }
+}
